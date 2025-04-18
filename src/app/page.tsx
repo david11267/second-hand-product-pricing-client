7f1d5c8cb5bdfd35ee35c000459c0b85b9dc77ec
David Aslan <davidaslan112671@live.se>
2025-04-18 23:35:01 +0200

    some minor table changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 export default function Home() {
   return (
-    <div className=" shadow-lg  bg-panel/60 rounded-4xl overflow-hidden">
-      <table className=" table-auto">
-        <thead className="bg-transparent">
+    <div className="  shadow-lg  bg-panel/60 rounded-4xl overflow-hidden p-4 inline-flex">
+      <table className=" table-auto ">
+        <thead className=" bg-transparent border-b border-dashed">
           <tr>
+            <td>
+              <input type="checkbox" />
+            </td>
             <th>Song</th>
             <th>Artist</th>
             <th>Year</th>
@@ -11,16 +14,25 @@ export default function Home() {
         </thead>
         <tbody>
           <tr>
+            <td>
+              <input type="checkbox" />
+            </td>
             <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
             <td>Malcolm Lockyer</td>
             <td>1961</td>
           </tr>
           <tr>
+            <td>
+              <input type="checkbox" />
+            </td>
             <td>Witchy Woman</td>
             <td>The Eagles</td>
             <td>1972</td>
           </tr>
           <tr>
+            <td>
+              <input type="checkbox" />
+            </td>
             <td>Shining Star</td>
             <td>Earth, Wind, and Fire</td>
             <td>1975</td>
